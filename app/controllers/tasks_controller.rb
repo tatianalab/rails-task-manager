9d4df210a9cb1eee26039928d11246bcacadc23b
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-10 17:01:24 -0500

    added bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -21,11 +21,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     # no need for app/views/restaurants/update.html.erb
@@ -33,17 +31,20 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details, :complete)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
