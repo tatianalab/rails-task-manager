35585559cd847970c515326605d45031735df920
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-10 15:17:00 -0500

    destroy method added - edit anchor link fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,26 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
