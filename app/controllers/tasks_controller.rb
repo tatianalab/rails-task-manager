33d5dba51f23b2da06196bd6774cacadff455f67
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-10 13:54:16 -0500

    create action added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+
 end
